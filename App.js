47e7b1606a040c4e42c565aa063402cf7905455c
Diep Nguyen <diep.ngtran96@gmail.com>
2020-06-13 20:27:49 +0700

    init commit: first hello world react native app

---
+++ App.js
@@ -0,0 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function HelloWorldApp() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello, world!</Text>
+    </View>)
+}
+export default HelloWorldApp
